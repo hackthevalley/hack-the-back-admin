1d4b4d965c4e4aa8ce45cbd353433bf965d55d10
Manpreet Bhatti <manpreet@bhatti.net>
2021-09-30 16:48:58 -0400

    Finished showing stats

--- src/pages/Home/ApplicationStats.jsx
+++ src/pages/Home/ApplicationStats.jsx
@@ -7,12 +7,18 @@ export default function ApplicationStats() {
   const { data: hackerApps } = useGet({
     path: '/api/admin/forms/hacker_application/responses/overview',
   });
+  const { data: accepted } = useGet({
+    path: '/api/admin/forms/hacker_application/responses?applicant__status=ACCEPTED_INVITE',
+  });
+  const { data: waiting } = useGet({
+    path: '/api/admin/forms/hacker_application/responses?applicant__status=WAITLISTED',
+  });
 
   let appCount = 0; // Counter for number of total applications
 
   return (
     <>
-      {hackerApps ? (
+      {hackerApps && accepted && waiting ? (
         <Box as={Link} to="/apps" p={5} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
           <Flex>
             <Box>
@@ -24,7 +30,18 @@ export default function ApplicationStats() {
                 {hackerApps.overview.forEach((i) => {
                   appCount += i.count;
                 })}
-                <StatNumber fontSize="md">{appCount}</StatNumber>
+                <StatNumber fontSize="md">{appCount} total</StatNumber>
+              </Stat>
+              <Stat>
+                {/* Shows 12 latest applied applicants */}
+                {accepted.count !== 1 ? (
+                  <StatNumber fontSize="md">{accepted.count} accepted invites</StatNumber>
+                ) : (
+                  <StatNumber fontSize="md">{accepted.count} accepted invite</StatNumber>
+                )}
+              </Stat>
+              <Stat>
+                <StatNumber fontSize="md">{waiting.count} waitlisted</StatNumber>
               </Stat>
             </Box>
           </Flex>
