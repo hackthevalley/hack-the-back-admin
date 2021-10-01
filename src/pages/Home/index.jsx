ae07bba31ea4f10d0df183cc12ecaa5e44801632
Manpreet Bhatti <manpreet@bhatti.net>
2021-10-01 00:18:04 -0400

    Finished donut chart for hacker applications

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
 import { useGet } from 'restful-react';
 
-import { Heading, HStack } from '@chakra-ui/react';
+import { Box, Center, Heading, HStack, Spinner, StackDivider, VStack } from '@chakra-ui/react';
 
 import DashboardContent from '../../components/DashboardContent';
+import DonutGraph from '../../components/DonutGraph';
 import ApplicationStats from './ApplicationStats';
 import UserStats from './UserStats';
 
@@ -10,15 +11,38 @@ export default function DashboardHome() {
   const { data: admin } = useGet({
     path: '/api/account/users/me',
   });
+  const { data: hackerApps } = useGet({
+    path: '/api/admin/forms/hacker_application/responses/overview',
+  });
+
+  const responseData = []; // Hacker application response counts
+  const responseLabels = []; // Hacker application response labels
 
   return (
     <>
-      {admin ? (
+      {admin && hackerApps ? (
         <DashboardContent title={`Hello there, ${admin.firstName}!`}>
-          <HStack spacing={8}>
-            <UserStats />
-            <ApplicationStats />
-          </HStack>
+          <VStack divider={<StackDivider />} spacing={8} align="stretch">
+            <Box>
+              <HStack spacing={8}>
+                <UserStats />
+                <ApplicationStats />
+              </HStack>
+            </Box>
+            <Box>
+              <Center>
+                {hackerApps.overview.forEach((i) => {
+                  responseData.push(i.count);
+                  responseLabels.push(i.status);
+                })}
+                {responseData.length !== 0 ? (
+                  <DonutGraph responseData={responseData} responseLabels={responseLabels} />
+                ) : (
+                  <Spinner />
+                )}
+              </Center>
+            </Box>
+          </VStack>
         </DashboardContent>
       ) : (
         <Heading>Loading...</Heading>
