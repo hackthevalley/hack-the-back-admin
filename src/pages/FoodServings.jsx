3bf113d0d27e2bc6ef4cfb37fd2bb52be4337504
Shreyas Rao <raoshreyas2004@gmail.com>
2024-09-27 20:14:06 -0400

    users can now update the currently served food

--- src/pages/FoodServings.jsx
+++ src/pages/FoodServings.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useGet } from 'restful-react';
+import { useGet, useMutate } from 'restful-react';
 
 import {
   Box,
@@ -19,13 +19,19 @@ import {
 
 export default function FoodServings() {
   const [selectedDay, setSelectedDay] = useState('');
-  const [selectedMeals, setSelectedMeals] = useState('');
+  const [selectedMeal, setSelectedMeal] = useState('');
   const [nowServing, setNowServing] = useState('');
+  const [requestedFoodId, setRequestedFoodId] = useState('');
   const [alert, setAlert] = useState({ isVisible: false, message: '' });
 
   const { data: allFoodData } = useGet({
     path: '/api/admin/food',
     verb: 'GET',
+  });
+
+  const { mutate: servingSwitch } = useMutate({
+    path: `/api/admin/food/${requestedFoodId}`,
+    verb: 'PATCH',
   });
 
   useEffect(() => {
@@ -46,18 +52,35 @@ export default function FoodServings() {
   };
 
   const handleMealChange = (meal) => {
-    setSelectedMeals(meal);
+    setSelectedMeal(meal);
+    const data = allFoodData.allFood.filter(
+      (food) => food.day === parseInt(selectedDay, 10) && food.name === meal
+    );
+    setRequestedFoodId(data[0].id);
   };
 
-  const handleSubmit = () => {
-    if (selectedDay && selectedMeals) {
-      setAlert({
-        isVisible: true,
-        message: `You selected Day ${selectedDay} ${selectedMeals}`,
-      });
-      console.log('day', selectedDay, 'meal', selectedMeals);
+  const handleSubmit = async () => {
+    if (selectedDay && selectedMeal) {
+      try {
+        // Update the backend
+        await servingSwitch();
+
+        setAlert({
+          type: 'success',
+          isVisible: true,
+          message: `You selected Day ${selectedDay} ${selectedMeal}`,
+        });
+      } catch (error) {
+        console.log(error);
+        setAlert({
+          type: 'error',
+          isVisible: true,
+          message: 'There was an error updating the food servings.',
+        });
+      }
     } else {
       setAlert({
+        type: 'error',
         isVisible: true,
         message: 'Please select both a day and a meal before updating.',
       });
@@ -76,7 +99,7 @@ export default function FoodServings() {
     switch (selectedDay) {
       case '1':
         return (
-          <RadioGroup value={selectedMeals} onChange={(value) => handleMealChange(value)}>
+          <RadioGroup value={selectedMeal} onChange={(value) => handleMealChange(value)}>
             <Stack direction="column">
               <Radio value="lunch">Lunch</Radio>
               <Radio value="dinner">Dinner</Radio>
@@ -85,7 +108,7 @@ export default function FoodServings() {
         );
       case '2':
         return (
-          <RadioGroup value={selectedMeals} onChange={(value) => handleMealChange(value)}>
+          <RadioGroup value={selectedMeal} onChange={(value) => handleMealChange(value)}>
             <Stack direction="column">
               <Radio value="breakfast">Breakfast</Radio>
               <Radio value="lunch">Lunch</Radio>
@@ -95,7 +118,7 @@ export default function FoodServings() {
         );
       case '3':
         return (
-          <RadioGroup value={selectedMeals} onChange={(value) => handleMealChange(value)}>
+          <RadioGroup value={selectedMeal} onChange={(value) => handleMealChange(value)}>
             <Stack direction="column">
               <Radio value="breakfast">Breakfast</Radio>
               <Radio value="lunch">Lunch</Radio>
@@ -121,7 +144,7 @@ export default function FoodServings() {
         <Box>
           <Heading as="h2" size="md" mb={7}>
             Selected Meal:
-            {selectedDay ? ` Day ${selectedDay} ${selectedMeals}` : ' No option selected'}
+            {selectedDay ? ` Day ${selectedDay} ${selectedMeal}` : ' No option selected'}
           </Heading>
         </Box>
         <Box mb={6}>
@@ -151,9 +174,9 @@ export default function FoodServings() {
 
         {/* Alert Box */}
         {alert.isVisible && (
-          <Alert status={selectedDay && selectedMeals ? 'success' : 'error'} mt={4}>
+          <Alert status={selectedDay && selectedMeal ? 'success' : 'error'} mt={4}>
             <AlertIcon />
-            <AlertTitle>{selectedDay && selectedMeals ? 'Success' : 'Error'}</AlertTitle>
+            <AlertTitle>{selectedDay && selectedMeal ? 'Success' : 'Error'}</AlertTitle>
             <AlertDescription>{alert.message}</AlertDescription>
             <CloseButton position="absolute" right="8px" top="8px" onClick={handleCloseAlert} />
           </Alert>
