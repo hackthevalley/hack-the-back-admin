f325b1d75188265eb85747a58ccebde3b7f539a9
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2025-08-13 01:54:41 +0200

    show all applicants

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -1,21 +1,51 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/utils/auth";
 import { useNavigate } from "react-router";
 import NavMenu from "@/components/Navmenu";
-import { Applicants } from "@/components/Applicants";
+import { Applicants, ApplicantProps } from "@/components/Applicants";
+import fetchInstance from "@/utils/api";
 
 function Apps() {
   const { isAuthenticated } = useContext(UserContext) ?? {};
+  const [applicants, setApplicants] = useState<ApplicantProps[]>([]);
   const navigate = useNavigate();
+
+  const getAllApps = async (ofs = 0, limit = 15): Promise<ApplicantProps[]> => {
+    try {
+      const data = await fetchInstance(
+        `admin/account/getallapps?ofs=${ofs}&limit=${limit}`,
+        { method: "GET" }
+      );
+      console.log(data.application);
+      setApplicants(
+        data.application.map((app: any) => ({
+          first_name: app.first_name,
+          last_name: app.last_name,
+          email: app.email ?? "",
+          status: app.status ?? "unknown",
+          created_at: app.created_at ?? "unknown",
+          updated_at: app.updated_at ?? "unknown",
+        }))
+      );
+      console.log(applicants);
+      return applicants;
+    } catch (error) {
+      console.error("Error fetching applicants:", error);
+      throw error;
+    }
+  };
+
   useEffect(() => {
     if (!isAuthenticated) {
       navigate("/login");
     }
+    getAllApps().catch((err) => console.log(err.message));
   }, [isAuthenticated, navigate]);
+
   return (
     <div className="flex h-screen">
       <NavMenu />
-      <Applicants />
+      <Applicants applicants={applicants} />
     </div>
   );
 }
