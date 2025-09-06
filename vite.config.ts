5b591e2cdc86fd5288b3dd7404f7e3fdd152187f
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-09-06 19:38:45 -0400

    Fix build on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
