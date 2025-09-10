eafaf8e977793568ad7cd04e379ac80ec27d6acd
Preston Tom-Ying <preston.tomying@gmail.com>
2025-09-09 20:38:45 -0400

    silly linting issue

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { UserContext } from "@/utils/auth";
 import { Button } from "./ui/button";
 import { House, Newspaper, LogOut, Menu } from "lucide-react";
